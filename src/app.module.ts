a273a262a23174c0019325c22be5e17ab1675b9d
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2025-12-18 09:18:57 -0300

    realizando configurações necessarias para o deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Produto } from './produto/entities/produto.entity';
-import { Categoria } from './categoria/entities/categoria.entity';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { ProdutoModule } from './produto/produto.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_crm',
-      entities: [Produto, Categoria, Usuario],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     ProdutoModule,
     CategoriaModule,
